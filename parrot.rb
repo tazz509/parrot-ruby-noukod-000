7e89e7e6ee7bf203ff3fac1568165757f8f25ead
carly <carly@noukod.com>
2018-09-13 18:05:56 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase = Squawk!, bird = Pretty bird!)
+def parrot(phrase = "Squawk!", bird = "Pretty bird!")
 
   puts  "#{phrase}"
 
